b25f513a704c94ae8438257ef2952c75b9ee5a65
Dustin Iacobazzi Riecan <dmriecan@gmail.com>
2024-02-09 15:54:23 -0800

    Add fonts and initial css theming and reset styles.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import classNames from "clsx";
+import { Nunito_Sans, Caveat } from "next/font/google";
 import { ThemeProvider, initialTheme } from "../_hooks/useTheme";
 import "./normalize.min.css";
 import "./globals.css";
+
+const nunitoSans = Nunito_Sans({
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-nunito-sans"
+});
+
+const caveat = Caveat({
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-caveat"
+});
 
 type RootLayoutProps = {
   children?: React.ReactNode;
@@ -12,7 +26,10 @@ const RootLayout: React.FC<RootLayoutProps> = ({
 }) => {
   return (
     <ThemeProvider>
-      <html lang="en">
+      <html
+        lang="en"
+        className={`${caveat.variable} ${nunitoSans.variable}`}
+      >
         <body className={initialTheme}>
           {children}
         </body>
